e0ce1f250c2608b1cac5fc1991198c4727cfc7a0
nyfelix <nyfelix@gmail.com>
2017-09-15 21:28:39 +0200

    ready to walk

--- LegoFumu/src/main.ino
+++ LegoFumu/src/main.ino
@@ -1,12 +1,11 @@
 #include <Arduino.h>
-
 #include <Adafruit_MotorShield.h>
 
 Adafruit_MotorShield currentMotorShield = Adafruit_MotorShield();
 Adafruit_DCMotor* left = currentMotorShield.getMotor(2);
 Adafruit_DCMotor* right = currentMotorShield.getMotor(1);
 int speed = 0;
-int trim = -20;
+int trim = 0;
 String apiBuffer = "";
 
 void setup() {
@@ -21,23 +20,25 @@ void loop() {
 
 void handleSerialInstruction(String task, String value) {
    // Implemnation need to be done in the derived class for now
+   Serial.println(task + ":" + value);
    if (task == "trim") {
      trim = value.toInt();
    }
    if (task == "speed") {
      speed = value.toInt();
+     left->setSpeed(speed);
+     right->setSpeed(speed+trim);
+     left->run(FORWARD);
+     right->run(FORWARD);
    }
-   left->setSpeed(speed);
-   right->setSpeed(speed+trim);
-   left->run(BACKWARD);
-   right->run(BACKWARD);
-   if (task = "stop") {
-     left->setSpeed(0);
-     right->setSpeed(0);
+
+   if (task == "stop") {
+     left->run(RELEASE);
+     right->run(RELEASE);
    }
 }
 
-void      readSerial() {
+void readSerial() {
   while (Serial.available() > 0) {
     //workstate:'workstate',rate:'blinkrate'\n
     //workstate:1, rate:30\n
