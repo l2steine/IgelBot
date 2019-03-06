422ed1878aa230c90bde15bca8a2d075d2603018
StefanMartyBAMI19 <48091642+StefanMartyBAMI19@users.noreply.github.com>
2019-03-06 11:38:23 +0100

    Revert "WiFi für HSR"
    This reverts commit 12e9bbb0814232d662ffd79ace435c9892405c1b.

--- device-embedded/src/WiFiConfig.h
+++ device-embedded/src/WiFiConfig.h
@@ -1,8 +1,6 @@
 // WiFi parameters
-//char ssid[] = "FerdinandAir";
-//char password[] = "lychee2011";
-char ssid[] = "DigitalLab";
-char password[] = "digital42HSR";
+char ssid[] = "FerdinandAir";
+char password[] = "lychee2011";
 
 char ssid_igel[] = "IgelNet";
 char password_igel[] = "igel";
